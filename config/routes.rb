d0ceef55c0aa335bf06626860816b21658258cb8
Kevin Nguyen <nxgkhanh.itc@gmail.com>
2020-12-09 12:12:00 +1100

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'friends#index'
+  resources :friends
+
+
+  get 'home/about'
+  # root 'home#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
